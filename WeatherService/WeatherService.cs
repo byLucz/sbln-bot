4b588bc8a028e1a3cc37f183280d5fc4d2501652
lucz <lucz@lois.media>
2026-09-15 20:25:50 +0300

    fix: miss cast string

--- WeatherService/WeatherService.cs
+++ WeatherService/WeatherService.cs
@@ -27,7 +27,7 @@ public class WeatherSer
 
 public class WeatherModel
 {
-    public string id { get; set; }
+    public int id { get; set; }
     public string main { get; set; }
     public string description { get; set; }
     public string icon { get; set; }
